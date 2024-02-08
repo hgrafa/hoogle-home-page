54e1b0e2fa1eaa76e754d85f3543f8febafe8228
hgrafa <hugorafell@gmail.com>
2024-02-08 13:19:14 -0300

    adding initial page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import Image from 'next/image'
+
 export default function Home() {
-  return <p>Hello World</p>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div></div>
+
+      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 text-xl backdrop-blur-2xl duration-300 ease-in-out hover:scale-105 dark:border-neutral-800 dark:bg-zinc-800/30  dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        Coming soon 🚀
+      </p>
+      {/* <div className="before:bg-gradient-radial after:bg-gradient-conic relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      </div> */}
+
+      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white hover:scale-110 dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          By Hoogle Education
+        </div>
+      </div>
+    </main>
+  )
 }
